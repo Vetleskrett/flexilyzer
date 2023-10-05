ba53295091f173e56c5fa1ca5ecf21f927b01303
Jacob Theisen <jacobtheisen2211@gmail.com>
2023-10-05 10:00:03 +0200

    refactor

---
+++ app/backend/mq_services/db_handler/db_handler.py
@@ -0,0 +1,13 @@
+import asyncio
+from prisma import Prisma
+# from prisma.models import Course
+
+async def main() -> None:    
+    db = Prisma()
+    await db.connect()
+    courses = await db.course.delete_many()
+
+    await db.disconnect()
+
+if __name__ == '__main__':
+    asyncio.run(main())
