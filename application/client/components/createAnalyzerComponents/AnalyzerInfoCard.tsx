c5388bdfd4a6b5c3395063fd046542ed5ca31bf2
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-01-29 15:46:41 +0100

    more information step 1

---
+++ application/client/components/createAnalyzerComponents/AnalyzerInfoCard.tsx
@@ -0,0 +1,35 @@
+import { Kbd, ScrollShadow } from "@nextui-org/react";
+import React from "react";
+
+export default function InfoCardContent() {
+  return (
+    <div>
+      <h2 className="h2">Create an analyzer</h2>
+      <p>
+        By following the steps in this page, you will define and register your
+        own analyzer.
+      </p>
+      <h3 className="h3 mt-4">What is an analyzer?</h3>
+      An analyzer is essentially a Python script that is executed on this
+      application's server, ad-hoc or scheduled, which will generate a specific
+      report about a student project. The reports may then be viewed on a the
+      belonging student group's page.
+      <h3 className="h3 mt-4">What can an analyzer do?</h3>
+      There is few limitations to what can be done by an analyzer. It might
+      fetch a Git API and generate some statistics, fetch an external web page,
+      do some calculations or measures on student code, or something totally
+      different. <br />
+      <br /> There is also no limitations when it comes to external third-party
+      libraries used by the analyzer script. When uploading your script, you
+      will also provie a <Kbd>requirements.txt</Kbd> file, which ensures that
+      all required libraries are available whenever the script is run.
+      <h3 className="h3 mt-4">How should the analyzer script be structured?</h3>
+      Since this system is meant to handle user-generated scripts with a variety
+      of different functionality, we needed to define a common structure
+      regarding how both the input and the output variables are accessed and
+      returned. By following the steps in this analyzer generation form, we will
+      provide a shell template for how what script should look like, which you
+      may then use as a starting point when making your analyzer script.
+    </div>
+  );
+}
--- application/client/components/createAnalyzerComponents/BasicInfoStep.tsx
+++ application/client/components/createAnalyzerComponents/BasicInfoStep.tsx
@@ -1,35 +1,67 @@
-import { Input, Textarea } from "@nextui-org/react"; // Assuming Next UI components
+import {
+  Accordion,
+  AccordionItem,
+  Card,
+  Input,
+  ScrollShadow,
+  Textarea,
+} from "@nextui-org/react"; // Assuming Next UI components
 
 import { SummaryStepProps } from "@/app/types/analyzerDefinitions";
+import InfoCardContent from "./AnalyzerInfoCard";
 
 const BasicInfoStep = ({ formData, setFormData }: SummaryStepProps) => {
   const updateFormData = (key: string, value: string) => {
     setFormData((prev) => ({ ...prev, [key]: value }));
   };
 
-  return (
-    <>
-      <h2 className="h2">Basic Info</h2>
-      <Input
-        isRequired
-        size="lg"
-        label="Name"
-        placeholder="Analyzer Name"
-        value={formData.name}
-        className="max-w-xs mb-8"
-        onChange={(e) => updateFormData("name", e.target.value)}
-      />
+  const infoCardContent = "Your info content here...";
 
-      <Textarea
-        fullWidth
-        size="lg"
-        label="Description"
-        value={formData.description}
-        placeholder="Analyzer Description"
-        className="max-w-xs"
-        onChange={(e) => updateFormData("description", e.target.value)}
-      />
-    </>
+  return (
+    <div className="flex flex-col sm:flex-row gap-4">
+      {/* Accordion for narrow screens */}
+      <div className="sm:w-1/2 mb-4 md:mb-0">
+        <div className="sm:hidden">
+          <Accordion>
+            <AccordionItem title="Useful information">
+              <InfoCardContent />
+            </AccordionItem>
+          </Accordion>
+        </div>
+
+        {/* Static Info Card for larger screens */}
+
+        <Card className="hidden sm:block p-4 max-h-screen-minus-navbar overflow-auto">
+          <ScrollShadow hideScrollBar>
+            <InfoCardContent />
+          </ScrollShadow>
+        </Card>
+      </div>
+
+      {/* Form Fields */}
+      <div className="flex-grow">
+        <h2 className="h2">Name and description</h2>
+
+        <Input
+          isRequired
+          size="lg"
+          label="Name"
+          placeholder="Analyzer Name"
+          value={formData.name}
+          className="mb-8"
+          onChange={(e) => updateFormData("name", e.target.value)}
+        />
+
+        <Textarea
+          size="lg"
+          label="Description"
+          placeholder="Analyzer Description"
+          value={formData.description}
+          className="mb-8"
+          onChange={(e) => updateFormData("description", e.target.value)}
+        />
+      </div>
+    </div>
   );
 };
 
