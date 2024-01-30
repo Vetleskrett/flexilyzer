9b519b5640d4d95829d4ccf31d0948c0329620aa
Jacob Theisen <jacobtheisen2211@gmail.com>
2024-01-30 16:36:20 +0100

    sui

--- application/client/app/analyzers/page.tsx
+++ application/client/app/analyzers/page.tsx
@@ -4,16 +4,16 @@ import { CreateAnalyzerButton } from "@/components/buttons";
 import { useRouter } from "next/navigation";
 
 export default async function Analyzers() {
-  const analyzers = await api.getAllAnalyzers();
+  const analyzers = await api.getAllAnalyzers({ cache: "no-cache" });
 
   console.log(analyzers);
   return (
     <div>
-      <h2 className="h2">All Analyzers:</h2>
+      <h2 className='h2'>All Analyzers:</h2>
       {analyzers.data.map((analyzer) => {
         return <AnalyzerOverview analyzer={analyzer} />;
       })}
-      <div className="flex flex-col items-center mt-30">
+      <div className='flex flex-col items-center mt-30'>
         <CreateAnalyzerButton />
       </div>
     </div>
