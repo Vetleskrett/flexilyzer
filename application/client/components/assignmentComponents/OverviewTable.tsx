2879ae942f278f8e14d4837f0df32ad47db31186
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-02-19 15:34:59 +0100

    table overview progress

--- application/client/components/assignmentComponents/OverviewTable.tsx
+++ application/client/components/assignmentComponents/OverviewTable.tsx
@@ -1,4 +1,8 @@
 "use client";
+import {
+  ReportTeamResponse,
+  ValueTypesOutput,
+} from "@/extensions/data-contracts";
 import { AnalyzerWithOutputs, TeamReports } from "@/types/analyzerDefinitions";
 import {
   Table,
@@ -7,13 +11,24 @@ import {
   TableColumn,
   TableRow,
   TableCell,
+  Chip,
+  Progress,
+  Tooltip,
 } from "@nextui-org/react";
-import { ReactElement } from "react";
 
 interface OverviewTableParams {
   analyzersWithOutputs: AnalyzerWithOutputs[];
   allReports: TeamReports;
 }
+
+type FlatMappedOutputs = {
+  analyzerId: number;
+  key_name: string;
+  value_type: ValueTypesOutput;
+  display_name?: string | null | undefined;
+  extended_metadata?: null | undefined;
+  id: number;
+};
 
 export default function OverviewTable({
   allReports,
@@ -22,33 +37,121 @@ export default function OverviewTable({
   console.log(allReports);
   console.log(analyzersWithOutputs);
 
+  // Creating a new array with flatMapped outputs from all analyzers
+  let flatMappedOutputs: FlatMappedOutputs[] = analyzersWithOutputs.flatMap(
+    (analyzer) =>
+      analyzer.outputs.map((output) => {
+        return {
+          ...output,
+          analyzerId: analyzer.id,
+        };
+      })
+  );
+
+  flatMappedOutputs = flatMappedOutputs.filter(
+    (output) => output.key_name !== "test_coverage"
+  );
+
+  const columnHeaders = [
+    <TableColumn key="team">Team</TableColumn>,
+    ...flatMappedOutputs.map((output) => (
+      <TableColumn key={output.id}>
+        {output.display_name ? output.display_name : output.key_name}
+      </TableColumn>
+    )),
+  ];
+
+  console.log(columnHeaders);
+
+  // Pre-define the rows
+  const rows = Object.entries(allReports).map(([teamId, teamData]) => {
+    const cells = [
+      <TableCell key={`team-${teamId}`}>{teamId}</TableCell>,
+      ...flatMappedOutputs.map((output) => {
+        // Find the corresponding report for the current output's analyzerId
+        const report = teamData.reports.find(
+          (r: ReportTeamResponse) => r.analyzer_id === output.analyzerId
+        );
+        // Attempt to find the value in the report using the key_name
+        const value = report ? report.report[output.key_name] : undefined;
+        console.log(value);
+        switch (output.value_type) {
+          case ValueTypesOutput.Range:
+            interface RangeMetadata {
+              fromRange: number;
+              toRange: number;
+            }
+
+            const extendedMetadata =
+              output.extended_metadata as unknown as RangeMetadata;
+
+            return (
+              <TableCell>
+                {value && (
+                  <Tooltip
+                    delay={0}
+                    closeDelay={0}
+                    content={
+                      <>
+                        {value} / {extendedMetadata.toRange}
+                      </>
+                    }
+                  >
+                    <Progress
+                      aria-label={output.key_name}
+                      size="md"
+                      value={value}
+                      minValue={extendedMetadata.fromRange}
+                      maxValue={extendedMetadata.toRange}
+                      color={
+                        value / extendedMetadata.toRange > 0.65
+                          ? "success"
+                          : "warning"
+                      }
+                      className="max-w-md"
+                    />
+                  </Tooltip>
+                )}
+              </TableCell>
+            );
+          case ValueTypesOutput.Str:
+            return <TableCell>{value}</TableCell>;
+          case ValueTypesOutput.Bool:
+            return (
+              <TableCell>
+                {value !== undefined && (
+                  <Chip
+                    size="sm"
+                    variant="solid"
+                    color={(value as boolean) ? "success" : "danger"}
+                    className="text-white"
+                  >
+                    {value ? "Yes" : "No"}
+                  </Chip>
+                )}
+              </TableCell>
+            );
+          case ValueTypesOutput.Int:
+            return <TableCell>{value}</TableCell>;
+          default:
+            return <TableCell>N/A</TableCell>;
+          // return (
+          //   <TableCell key={`${teamId}-${output.analyzerId}-${output.key_name}`}>
+          //     {value || ""}
+          //   </TableCell>
+          // );
+        }
+      }),
+    ];
+
+    return <TableRow key={teamId}>{cells}</TableRow>;
+  });
+
   return (
     <>
       <Table>
-        <TableHeader>
-          <TableColumn key={"teams"}>Team</TableColumn>
-          <>
-            {analyzersWithOutputs.flatMap((analyzer) =>
-              analyzer.outputs.map((output) => {
-                return (
-                  <TableColumn key={output.id}>
-                    {output.display_name
-                      ? output.display_name
-                      : output.key_name}
-                  </TableColumn>
-                );
-              })
-            )}
-          </>
-        </TableHeader>
-
-        <TableBody>
-          {Object.entries(allReports).map(([teamId, { reports }]) => (
-            <TableRow key={teamId}>
-              <TableCell>{teamId}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+        <TableHeader>{columnHeaders}</TableHeader>
+        <TableBody>{rows}</TableBody>
       </Table>
     </>
   );
