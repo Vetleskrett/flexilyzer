d133c19964f0a2e4a4cb8c1435694f9e4d3773b8
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-02-19 12:07:42 +0100

    minor

--- application/client/app/courses/[course_id]/assignments/[assignment_id]/overview/page.tsx
+++ application/client/app/courses/[course_id]/assignments/[assignment_id]/overview/page.tsx
@@ -3,5 +3,5 @@ interface Props {
 }
 
 export default function AssignmentOverviewPage({ params }: Props) {
-  return <div className='flex flex-row justify-center'>haala baby</div>;
+  return <div className="flex flex-row justify-center">haala baby</div>;
 }
