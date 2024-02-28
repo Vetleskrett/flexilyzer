ffbf130ffec44808b25ab636b6e416da367036bc
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-02-28 11:06:11 +0100

    bug fix

--- application/client/components/InfoCard.tsx
+++ application/client/components/InfoCard.tsx
@@ -10,7 +10,7 @@ export const InfoCard = ({ header, body }: InfoCardProps) => {
     <div className="flex justify-center items-center mt-16 ">
       <Card className="flex flex-col items-center justify-center pt-10 pb-5 px-10 shadow-xl bg-yellow-100">
         <h3 className="h3 mb-4">{header}</h3>
-        <p className="text-center text-gray-500 mb-6">{body}</p>
+        <div className="text-center text-gray-500 mb-6">{body}</div>
       </Card>
     </div>
   );
