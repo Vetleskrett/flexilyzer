c90a98010654e7b1a9c75b4058c3c8a76466eac9
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-03-01 16:38:12 +0100

    add tooltip with description on each cell

--- application/client/components/tableComponents/renderCell.tsx
+++ application/client/components/tableComponents/renderCell.tsx
@@ -8,79 +8,119 @@ import { standardTimeFormatter } from "@/utils/timeUtils";
 import { Tooltip, Progress } from "@nextui-org/react";
 import { SmallBool } from "@/components/tableComponents/SmallBoolChip";
 
+type ExtendedValueObj = {
+  value: string | number | boolean | Date;
+  desc: string;
+};
+
+// Type guard for ExtendedValueObj
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isExtendedValueObj = (value: any): value is ExtendedValueObj => {
+  return (
+    value &&
+    typeof value === "object" &&
+    "value" in value &&
+    "desc" in value &&
+    typeof value.desc === "string"
+  );
+};
+
+type TooltipWrapperProps = {
+  desc: string | undefined;
+  children: JSX.Element;
+};
+
+const TooltipWrapper = ({ desc, children }: TooltipWrapperProps) => {
+  return desc ? (
+    <Tooltip delay={0} closeDelay={0} content={desc}>
+      {children}
+    </Tooltip>
+  ) : (
+    children
+  );
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const render = (value: any, outputDef: FlatMappedOutputs, desc?: string) => {
+  switch (outputDef.value_type) {
+    case ValueTypesOutput.Range:
+      const extendedMetadata = outputDef.extended_metadata as unknown as {
+        fromRange: number;
+        toRange: number;
+      };
+
+      return (
+        <TooltipWrapper desc={desc}>
+          <Progress
+            aria-label={outputDef.key_name}
+            size="md"
+            value={value as number}
+            minValue={extendedMetadata.fromRange}
+            maxValue={extendedMetadata.toRange}
+            color={
+              (value as number) / extendedMetadata.toRange > 0.65
+                ? "success"
+                : "warning"
+            }
+            className="max-w-md"
+          />
+        </TooltipWrapper>
+      );
+
+    case ValueTypesOutput.Str:
+      return (
+        <TooltipWrapper desc={desc}>
+          <div className="text-xs">{value as string}</div>
+        </TooltipWrapper>
+      );
+
+    case ValueTypesOutput.Date:
+      return (
+        <TooltipWrapper desc={desc}>
+          <div className="text-xs">{standardTimeFormatter(value as Date)}</div>
+        </TooltipWrapper>
+      );
+
+    case ValueTypesOutput.Bool:
+      return (
+        <TooltipWrapper desc={desc}>
+          <SmallBool text={value.toString()} isGreen={value as boolean} />
+        </TooltipWrapper>
+      );
+
+    case ValueTypesOutput.Int:
+      return (
+        <TooltipWrapper desc={desc}>
+          <div className="text-xs">{formatter.format(value as number)}</div>
+        </TooltipWrapper>
+      );
+
+    default:
+      return "N/A";
+  }
+};
+
 const renderCell = (
   item: ReportTeamResponse[],
   columnKey: string,
   flatMappedOutputs: FlatMappedOutputs[],
 ) => {
   const [name, id] = columnKey.split("-");
-
   if (name === "Team") return item[0].team_id;
+
   const report = item.find(
     (r: ReportTeamResponse) => r.analyzer_id === Number(id),
   );
   const value = report ? report.report[name] : undefined;
-
-  const output = flatMappedOutputs.find(
-    (output) => output.analyzerId === Number(id) && output.key_name === name,
+  const outputDef = flatMappedOutputs.find(
+    (o) => o.analyzerId === Number(id) && o.key_name === name,
   );
-  if (!output) return value;
 
-  if (value === undefined) {
-    return <p className="font-light">-</p>;
-  }
-  switch (output.value_type) {
-    case ValueTypesOutput.Range:
-      interface RangeMetadata {
-        fromRange: number;
-        toRange: number;
-      }
+  if (!outputDef || value === undefined) return <p className="font-light">-</p>;
 
-      const extendedMetadata =
-        output.extended_metadata as unknown as RangeMetadata;
-
-      return (
-        (value || value === 0) && (
-          <Tooltip
-            delay={0}
-            closeDelay={0}
-            content={
-              <>
-                {value} / {extendedMetadata.toRange}
-              </>
-            }
-          >
-            <Progress
-              aria-label={output.key_name}
-              size="md"
-              value={value}
-              minValue={extendedMetadata.fromRange}
-              maxValue={extendedMetadata.toRange}
-              color={
-                value / extendedMetadata.toRange > 0.65 ? "success" : "warning"
-              }
-              className="max-w-md"
-            />
-          </Tooltip>
-        )
-      );
-    case ValueTypesOutput.Str:
-      return <div className="text-xs">{value}</div>;
-    case ValueTypesOutput.Date:
-      return (
-        value && <div className="text-xs">{standardTimeFormatter(value)}</div>
-      );
-    case ValueTypesOutput.Bool:
-      return (
-        value !== undefined && (
-          <SmallBool text={value.toString()} isGreen={value} />
-        )
-      );
-    case ValueTypesOutput.Int:
-      return value && <div className="text-xs">{formatter.format(value)}</div>;
-    default:
-      return "N/A";
-  }
+  return isExtendedValueObj(value)
+    ? render(value.value, outputDef, value.desc)
+    : render(value, outputDef);
 };
 
 export { renderCell };
