2575e5b49fccfc4fb85e10c40fcaf5ffd388908a
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-03-04 11:32:31 +0100

    update to handle zip inputs

--- application/client/components/createAnalyzerComponents/InputParamsStep.tsx
+++ application/client/components/createAnalyzerComponents/InputParamsStep.tsx
@@ -8,7 +8,12 @@ const InputParameters = ({ formData, setFormData }: SummaryStepProps) => {
   const updateInputParameter = (index: number, key: string, value: string) => {
     const updatedParameters = formData.inputs.map((param, i) => {
       if (i === index) {
-        return { ...param, [key]: value };
+        // If value_type is "zip", key_name should be set to a default key_name accordingly
+        if (key === "value_type" && value === ValueTypesInput.Zip) {
+          return { ...param, [key]: value, key_name: "zip_delivery_path" };
+        } else {
+          return { ...param, [key]: value };
+        }
       }
       return param;
     });
@@ -37,6 +42,7 @@ const InputParameters = ({ formData, setFormData }: SummaryStepProps) => {
       inputs: prev.inputs.filter((_, i) => i !== index),
     }));
   };
+
   return (
     <>
       <div className="pb-16">
@@ -44,11 +50,13 @@ const InputParameters = ({ formData, setFormData }: SummaryStepProps) => {
         {formData.inputs.map((param, index) => (
           <div key={param.id} className="mb-4 flex items-center space-x-2">
             <Input
+              isDisabled={param.value_type === ValueTypesInput.Zip}
               isRequired
               label="Key Name"
               placeholder="Enter key name"
               value={param.key_name}
               onChange={(e) => {
+                console.log("here");
                 // Regular expression: starts with a lowercase letter, followed by any mix of lowercase, uppercase letters, or underscores
                 const isValid = /^[a-z][a-zA-Z_]*$/.test(e.target.value);
                 if (isValid || e.target.value === "") {
@@ -58,6 +66,13 @@ const InputParameters = ({ formData, setFormData }: SummaryStepProps) => {
             />
 
             <Select
+              disabledKeys={
+                formData.inputs.some(
+                  (input) => input.value_type === ValueTypesInput.Zip,
+                )
+                  ? [ValueTypesInput.Zip]
+                  : []
+              }
               isRequired
               disallowEmptySelection
               label="Select value type"
@@ -68,15 +83,11 @@ const InputParameters = ({ formData, setFormData }: SummaryStepProps) => {
                 updateInputParameter(index, "value_type", e.target.value);
               }}
             >
-              <SelectItem key={"str"} value={"str"}>
-                str
-              </SelectItem>
-              <SelectItem key={"int"} value={"int"}>
-                int
-              </SelectItem>
-              <SelectItem key={"bool"} value={"bool"}>
-                bool
-              </SelectItem>
+              {Object.entries(ValueTypesInput).map((output) => (
+                <SelectItem key={output[1]} value={output[1]}>
+                  {output[0]}
+                </SelectItem>
+              ))}
             </Select>
             {/* Optional: Button to remove this parameter */}
             <Button color="danger" onClick={() => removeInputParameter(index)}>
