c92df3eaa57891e971ae6586c804ec5eac1f9926
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-03-05 10:27:23 +0100

    some basic tests for testing the test suite

---
+++ application/backend/test/testScripts/pythonTests/basic_tasks.py
@@ -0,0 +1,39 @@
+# Function that adds two numbers
+def add(a, b):
+    # Implement the addition and return the result
+    pass
+
+# Function that subtracts two numbers
+def subtract(a, b):
+    # Implement the subtraction and return the result
+    pass
+
+# Function that multiplies two numbers
+def multiply(a, b):
+    # Implement the mulitplication and return the result
+    pass
+
+# Determine if a number is positive, negative, or zero
+def check_number(number):
+    # Implement the logic to check the number and return the result as a bool
+    pass
+
+# Function that creates a list of continuous numbers from "start" value to "end" value
+def list_of_numbers(start, end):
+    # Implement logic and return the list
+    pass
+
+# Function that duplicates all letters in a string, e.g. "hello" -> "hheelllloo"
+def duplicate_letters(string):
+    # Implement logic and return the duplicated string
+    pass
+
+# Function that removes all vowels from a string
+def remove_vowels(string):
+    # Implement logic and return the modifified string
+    pass
+
+# Function that moves the first element of the list to the end of the list
+def modify_list(input_list):
+    # Implement the logic that modifies the list and returns it
+    pass
