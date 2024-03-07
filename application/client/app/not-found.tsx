6ac0cbe2b3694c37f8d56248ac2541358b195128
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-03-07 10:17:32 +0100

    double quotes

--- application/client/app/not-found.tsx
+++ application/client/app/not-found.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className='mt-16 flex flex-col items-center justify-center gap-4'>
-      <h2 className='h2'>Not found</h2>
+    <div className="mt-16 flex flex-col items-center justify-center gap-4">
+      <h2 className="h2">Not found</h2>
       The page you were trying to access could not be found.
       <Link href={"/"}>
         <Button>Back to Home Page</Button>
