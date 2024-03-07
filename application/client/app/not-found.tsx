e8f2826c9d0a58e22b0ac06110f5894c890fc9bb
Petter Lauvrak <pettelau@stud.ntnu.no>
2024-03-07 09:50:23 +0100

    not found page

---
+++ application/client/app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@nextui-org/button";
+import Link from "next/link";
+const NotFound = () => {
+  return (
+    <div className="mt-16 flex flex-col items-center justify-center gap-4">
+      <h2 className="h2">Not found</h2>
+      The page you were trying to access could not be found.
+      <Link href={"/"}>
+        <Button>Back to Home Page</Button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
