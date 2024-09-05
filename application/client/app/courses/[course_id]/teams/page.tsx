98819b9a4ffa40b86c25b63c1f17651c2ff31dec
Erlend Matre <erlema@stud.ntnu.no>
2024-09-05 18:38:22 +0200

    Add functionality to add courses

--- application/client/app/courses/[course_id]/teams/page.tsx
+++ application/client/app/courses/[course_id]/teams/page.tsx
@@ -1,6 +1,9 @@
+"use client";
 import api from "@/utils/apiUtils";
 import CreateButton from "@/components/buttons/CreateButton";
 import TeamOverview from "@/components/teamComponents/TeamOverview";
+import { useSnackbar } from "@/context/snackbarContext";
+import { useRouter } from "next/navigation";
 
 interface Props {
   params: { course_id: string };
@@ -10,6 +13,25 @@ export default async function CourseTeamsPage({ params }: Props) {
   const course_details = await api.getCourse(Number(params.course_id));
 
   const course_teams = await api.getCourseTeams(Number(params.course_id));
+
+  const { openSnackbar } = useSnackbar();
+  const router = useRouter();
+
+  async function AddTeam() {
+    const resp = await api.postTeam({course_id: Number(params.course_id)});
+    if (resp.ok) {
+      openSnackbar({
+        message: "Analyzer submitted successfully!",
+        severity: "success",
+      });
+      router.refresh();
+    } else {
+      openSnackbar({
+        message: `Something wrong while submitting Analyzer: ${resp.error}`,
+        severity: "warning",
+      });
+    }
+  }
 
   return (
     <div className="ml-10 mt-10">
@@ -26,7 +48,7 @@ export default async function CourseTeamsPage({ params }: Props) {
       ))}
       <div className="flex justify-center">
         <CreateButton
-          pushRoute={`/courses/${params.course_id}/teams/new`}
+          onClickFunction={AddTeam}
           text={"Add Team"}
         />
       </div>
